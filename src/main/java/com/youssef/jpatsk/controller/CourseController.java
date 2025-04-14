6bb1e35997c3072da06b33bd1c438b80ae427e32
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-14 23:44:38 +0200

    feat: added author and service controllers

---
+++ src/main/java/com/youssef/jpatsk/controller/CourseController.java
@@ -0,0 +1,27 @@
+package com.youssef.jpatsk.controller;
+
+import com.youssef.jpatsk.entity.Course;
+import com.youssef.jpatsk.service.CourseService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<Course> getCourses(@RequestParam(defaultValue = "0") int page,
+                                   @RequestParam(defaultValue = "2") int limit) {
+        return courseService.findAll(page, limit);
+    }
+}
