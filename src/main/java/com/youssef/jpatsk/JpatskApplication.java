8d513f7d2dee9d3a43f6f68b6a703acbc7cf378f
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-14 21:20:38 +0200

    initial commit

---
+++ src/main/java/com/youssef/jpatsk/JpatskApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.jpatsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpatskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youssef/jpatsk/JpatskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.jpatsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpatskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
