97a96a3ebd4a84ec24df8d0764b206777223ce18
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-14 22:44:26 +0200

    feat: added entities

---
+++ src/main/java/com/youssef/jpatsk/entity/Assessment.java
@@ -0,0 +1,48 @@
+package com.youssef.jpatsk.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "T_ASSESSMENT")
+public class Assessment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "CONTENT")
+    private String content;
+
+    @OneToOne
+    @JoinColumn(name = "COURSE_ID")
+    private Course course;
+
+    public Assessment(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
---
+++ src/main/java/com/youssef/jpatsk/entity/Author.java
@@ -0,0 +1,79 @@
+package com.youssef.jpatsk.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "T_AUTHOR")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "BIRTH_DATE")
+    private Date birthDate;
+
+    @ManyToMany
+    @JoinTable(
+            name = "T_AUTHOR_COURSE",
+            joinColumns = @JoinColumn(name = "AUTHOR_ID"),
+            inverseJoinColumns = @JoinColumn(name = "COURSE_ID")
+    )
+    private Set<Course> courses;
+
+    public Author(Long id, String name, String email, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+}
