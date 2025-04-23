856cc40c46d52d4a90a3f19dfe388f9306ce2564
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-23 16:33:44 +0200

    feat: added user repository

---
+++ src/main/java/com/youssef/jpatsk/dao/UserRepository.java
@@ -0,0 +1,12 @@
+package com.youssef.jpatsk.dao;
+
+import com.youssef.jpatsk.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
