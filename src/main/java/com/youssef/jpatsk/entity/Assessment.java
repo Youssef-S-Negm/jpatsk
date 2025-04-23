2966ea2e914efa3d5faef9e6c73903b611650a71
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-23 15:00:54 +0200

    update: changed the id GenerationType from "AUTO" to "IDENTITY"

--- src/main/java/com/youssef/jpatsk/entity/Assessment.java
+++ src/main/java/com/youssef/jpatsk/entity/Assessment.java
@@ -8,7 +8,7 @@ import jakarta.persistence.*;
 public class Assessment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "CONTENT")
