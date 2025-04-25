7ade17fe61a9a0134e5e18631b800d89048c1e67
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-25 22:32:24 +0300

    update: used MySQL container in the course repository integration test

--- src/test/java/com/youssef/jpatsk/repository/CourseRepositoryIntegrationTest.java
+++ src/test/java/com/youssef/jpatsk/repository/CourseRepositoryIntegrationTest.java
@@ -2,10 +2,16 @@ package com.youssef.jpatsk.repository;
 
 import com.youssef.jpatsk.dao.CourseRepository;
 import com.youssef.jpatsk.entity.Course;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,13 +19,44 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-public class CourseRepositoryIntegrationTest {
+@Testcontainers
+class CourseRepositoryIntegrationTest {
+
+    @Container
+    static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>("mysql:8.0.36")
+            .withDatabaseName("courses_app")
+            .withUsername("root")
+            .withPassword("admin");
+
 
     private final CourseRepository repository;
 
     @Autowired
     public CourseRepositoryIntegrationTest(CourseRepository repository) {
         this.repository = repository;
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driverClassName", MY_SQL_CONTAINER::getDriverClassName);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+        registry.add("spring.jpa.hibernate.naming.physical-strategy",
+                () -> "org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl");
+    }
+
+    @BeforeEach
+    public void setup() {
+        Course course1 = new Course(1L, "Java Basics", "Introduction to Java programming.", 3);
+        Course course2 = new Course(2L, "Kotlin", "Intro to Kotlin.", 4);
+        Course course3 = new Course(3L, "Python", "Intro to python", 5);
+        List<Course> courses = List.of(course1, course2, course3);
+
+        MY_SQL_CONTAINER.start();
+
+        repository.saveAll(courses);
     }
 
     @Test
