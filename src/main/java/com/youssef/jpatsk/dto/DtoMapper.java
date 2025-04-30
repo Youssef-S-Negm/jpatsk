60a857d5dcb0b9a07671a9605c778a3573b96fbc
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-30 18:01:26 +0300

    update: added MapStruct mapping annotations

--- src/main/java/com/youssef/jpatsk/dto/DtoMapper.java
+++ src/main/java/com/youssef/jpatsk/dto/DtoMapper.java
@@ -3,17 +3,34 @@ package com.youssef.jpatsk.dto;
 import com.youssef.jpatsk.entity.Author;
 import com.youssef.jpatsk.entity.Course;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface DtoMapper {
 
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "description", source = "description")
+    @Mapping(target = "credit", source = "credit")
     List<CourseDto> coursesToCoursesDto(List<Course> courses);
 
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "birthDate", source = "birthDate")
     AuthorDto authorToAuthorDto(Author author);
 
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "description", source = "description")
+    @Mapping(target = "credit", source = "credit")
     CourseDto courseToDto(Course course);
 
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "description", source = "description")
+    @Mapping(target = "credit", source = "credit")
     Course dtoToCourse(CourseDto dto);
 }
