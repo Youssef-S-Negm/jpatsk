370aa3ce645d442a600be013ce80985d0cf5fca0
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-30 18:00:56 +0300

    update: followed endpoints naming conventions, added Swagger api responses

--- src/main/java/com/youssef/jpatsk/controller/CourseController.java
+++ src/main/java/com/youssef/jpatsk/controller/CourseController.java
@@ -1,7 +1,13 @@
 package com.youssef.jpatsk.controller;
 
 import com.youssef.jpatsk.dto.CourseDto;
+import com.youssef.jpatsk.dto.ErrorResponse;
 import com.youssef.jpatsk.service.CourseService;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,25 +24,74 @@ public class CourseController {
         this.courseService = courseService;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "List of courses",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = CourseDto.class)))
+                    }
+            ),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid page or limit format",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    })
+    })
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     public List<CourseDto> getCourses(@RequestParam(defaultValue = "0") int page,
                                       @RequestParam(defaultValue = "2") int limit) {
         return courseService.findAll(page, limit);
     }
 
-    @PostMapping("/add")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Add a course",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CourseDto.class))
+                    }
+            )
+    })
+    @PostMapping
     public ResponseEntity<CourseDto> addCourse(@RequestBody CourseDto course) {
         return new ResponseEntity<>(courseService.add(course), HttpStatus.CREATED);
     }
 
-    @PutMapping("/update/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Update course by ID",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CourseDto.class)
+                            )}
+            ),
+            @ApiResponse(responseCode = "404",
+                    description = "Provided course id doesn't exist",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    })
+    })
+    @PutMapping("/{id}")
     public ResponseEntity<CourseDto> updateCourse(@PathVariable Long id,
                                                   @RequestBody CourseDto course) {
         return ResponseEntity.ok(courseService.update(id, course));
     }
 
-    @DeleteMapping("/delete/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "Delete course by ID"
+            ),
+            @ApiResponse(responseCode = "404",
+                    description = "Provided course id doesn't exist",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    })
+    })
+    @DeleteMapping("/{id}")
     public ResponseEntity<CourseDto> deleteCourseById(@PathVariable Long id) {
         courseService.delete(id);
 
