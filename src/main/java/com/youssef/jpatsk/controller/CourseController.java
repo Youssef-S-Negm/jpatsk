bf68e41a0fdf0959323cd125d8b2e40de0b4282e
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-30 13:46:15 +0300

    update: dto mapping is applied in the service layer instead of the controller layer

--- src/main/java/com/youssef/jpatsk/controller/CourseController.java
+++ src/main/java/com/youssef/jpatsk/controller/CourseController.java
@@ -1,8 +1,6 @@
 package com.youssef.jpatsk.controller;
 
 import com.youssef.jpatsk.dto.CourseDto;
-import com.youssef.jpatsk.dto.DtoMapper;
-import com.youssef.jpatsk.entity.Course;
 import com.youssef.jpatsk.service.CourseService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,16 +14,14 @@ import java.util.List;
 public class CourseController {
 
     private final CourseService courseService;
-    private final DtoMapper mapper;
 
-    public CourseController(CourseService courseService, DtoMapper mapper) {
+    public CourseController(CourseService courseService) {
         this.courseService = courseService;
-        this.mapper = mapper;
     }
 
     @GetMapping
     public List<CourseDto> getCourses(@RequestParam(defaultValue = "0") int page,
                                       @RequestParam(defaultValue = "2") int limit) {
-        return mapper.coursesToCoursesDto(courseService.findAll(page, limit));
+        return courseService.findAll(page, limit);
     }
 }
