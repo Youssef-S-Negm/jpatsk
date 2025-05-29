cc6fdbe745cc4dee414813f7b6c00d544fd0f591
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-29 19:31:21 +0300

    feat: added todo entity

---
+++ src/main/java/com/youssef/jpatsk/entity/Todo.java
@@ -0,0 +1,65 @@
+package com.youssef.jpatsk.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "T_TODO")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "STATUS")
+    private String status;
+
+    @Column(name = "PRIORITY")
+    private int priority;
+
+    @Column(name = "DATE_CREATED")
+    private Date dateCreated;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
